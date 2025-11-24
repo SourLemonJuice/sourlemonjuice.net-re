7fed88f75f1cbd1dbabea807c0049650b8cd8fd9
酸柠檬猹/SourLemonJuice <SourLemonJuice233@outlook.com>
2025-11-25 01:13:20 +0800

    Update colors

--- src/components/Warning.svelte
+++ src/components/Warning.svelte
@@ -3,14 +3,14 @@
 </script>
 
 <p class="warning">
-	Warning: {@render content()}
+	{@render content()}
 </p>
 
 <style>
 	.warning {
 		margin: 8px 0 8px 0;
-		padding: 4px 12px;
+		padding: 4px 8px;
 		background-color: #f5f1f0;
-		border-left: 4px solid #cc6649;
+		border-left: 6px solid #cc6649;
 	}
 </style>
