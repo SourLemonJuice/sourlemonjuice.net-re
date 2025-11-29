6937dbcb619f51f8e2b40cdeb1abb86aece66477
酸柠檬猹/SourLemonJuice <SourLemonJuice233@outlook.com>
2025-11-30 05:03:22 +0800

    Add sitemap on /sitemap-index.xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import svelte from '@astrojs/svelte';
-
 import cloudflare from '@astrojs/cloudflare';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [svelte()],
+	site: 'https://sourlemonjuice.net',
+	integrations: [svelte(), sitemap()],
 	adapter: cloudflare(),
 });
